8027cc97b7333f6c9eb912770d1814a951bc0fb0
Rafazanella01 <rafazanella2004@gmail.com>
2024-05-13 02:05:05 -0300

    Update

--- SistemaMatriculas/src/main/java/com/mycompany/sistemamatriculas/DisciplinaEad.java
+++ SistemaMatriculas/src/main/java/com/mycompany/sistemamatriculas/DisciplinaEad.java
@@ -4,6 +4,23 @@ package com.mycompany.sistemamatriculas;
  *
  * @author rafaz
  */
-public class DisciplinaEad {
+public class DisciplinaEad extends Disciplina{
     
+    private int numeroUnidades;
+    
+    public DisciplinaEad(int codigo, String nome, String ementa, int numeroUnidades)
+    {
+        super(codigo, nome, ementa);
+        this.numeroUnidades = numeroUnidades;
+    }
+    
+    public int getNumeroUnidades()
+    {
+        return numeroUnidades;
+    }
+    
+    public void setNumeroUnidades(int numeroUnidades)
+    {
+        this.numeroUnidades = numeroUnidades;
+    }
 }
--- SistemaMatriculas/src/main/java/com/mycompany/sistemamatriculas/DisciplinaPresencial.java
+++ SistemaMatriculas/src/main/java/com/mycompany/sistemamatriculas/DisciplinaPresencial.java
@@ -4,6 +4,23 @@ package com.mycompany.sistemamatriculas;
  *
  * @author rafaz
  */
-public class DisciplinaPresencial {
+public class DisciplinaPresencial extends Disciplina{
     
+    private int numeroAulas;
+    
+    public DisciplinaPresencial(int codigo, String nome, String ementa, int numeroAulas)
+    {
+        super(codigo, nome, ementa);
+        this.numeroAulas = numeroAulas;
+    }
+    
+    public int getNumeroAulas()
+    {
+        return numeroAulas;
+    }
+    
+    public void setNumeroAulas(int numeroAulas)
+    {
+        this.numeroAulas = numeroAulas;
+    }
 }
